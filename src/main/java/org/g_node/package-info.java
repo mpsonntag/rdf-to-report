642821294e4a34c6811a57a0a20c9e18bfde2451
M. Sonntag <michael.p.sonntag@gmail.com>
2016-02-09 20:48:52 +0100

    Add main package-info file

---
+++ src/main/java/org/g_node/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2016, German Neuroinformatics Node (G-Node)
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted under the terms of the BSD License. See
+ * LICENSE file in the root of the Project.
+ *
+ * Provides a framework for extracting reports from an RDF file from the command line.
+ */
+
+package org.g_node;
