a2e79401aa0f0d257a5c11f99849e46e5dc5c27e
M. Sonntag <michael.p.sonntag@gmail.com>
2016-02-10 12:07:52 +0100

    Add LKTLogbook reporter package

---
+++ src/main/java/org/g_node/reporter/LKTLogbook/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright (c) 2016, German Neuroinformatics Node (G-Node)
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted under the terms of the BSD License. See
+ * LICENSE file in the root of the Project.
+ *
+ * Package contains all required classes for reports specific for the use case of Kay Thurley.
+ */
+
+package org.g_node.reporter.LKTLogbook;
