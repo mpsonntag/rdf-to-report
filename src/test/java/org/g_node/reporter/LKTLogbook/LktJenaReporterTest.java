65331cd7c8bb8750059879be723d585095efe98a
M. Sonntag <michael.p.sonntag@gmail.com>
2016-03-15 18:15:39 +0100

    Tests: Add LktJenaReporter test file

---
+++ src/test/java/org/g_node/reporter/LKTLogbook/LktJenaReporterTest.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright (c) 2016, German Neuroinformatics Node (G-Node)
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted under the terms of the BSD License. See
+ * LICENSE file in the root of the Project.
+ */
+
+package org.g_node.reporter.LKTLogbook;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+import com.hp.hpl.jena.query.QueryParseException;
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.g_node.App;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Unit tests for the {@link LktJenaReporter} class. Output and Error streams are redirected
+ * from the console to a different PrintStream and reset after tests are finished
+ * to avoid mixing tool error messages with actual test error messages.
+ *
+ * @author Michael Sonntag (sonntag@bio.lmu.de)
+ */
+public class LktJenaReporterTest {
+
+    private ByteArrayOutputStream outStream;
+    private PrintStream stdout;
+
+    private final String tmpRoot = System.getProperty("java.io.tmpdir");
+    private final String testFolderName = "LktCliControllerTest";
+    private final Path testFileFolder = Paths.get(tmpRoot, testFolderName);
+    private final String testRdfFileName = "test.ttl";
+    private final File testRdfFile = this.testFileFolder.resolve(this.testRdfFileName).toFile();
+
+    /**
+     * Redirect Error and Out stream. Set up temporary folder and minimal RDF file.
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        final String miniTTL = "@prefix foaf: <http://xmlns.com/foaf/0.1/> . _:a foaf:name \"MainName\"";
+        FileUtils.write(this.testRdfFile, miniTTL);
+
+        this.stdout = System.out;
+        this.outStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(this.outStream));
+
+        Logger rootLogger = Logger.getRootLogger();
+        rootLogger.setLevel(Level.INFO);
+        rootLogger.addAppender(
+                new ConsoleAppender(
+                        new PatternLayout("[%-5p] %m%n")
+                )
+        );
+    }
+
+    /**
+     * Reset Out stream to the console after the tests are done. Remove all testfiles and temporary folder.
+     * @throws Exception
+     */
+    @After
+    public void tearDown() throws Exception {
+        System.setOut(this.stdout);
+
+        if (Files.exists(this.testFileFolder)) {
+            FileUtils.deleteDirectory(this.testFileFolder.toFile());
+        }
+    }
+
+    @Test
+    public void testRunReporterInvalidQuery() throws Exception {
+        final String invalidQuery = "I am not a SPARQL query";
+        final String outFile = "out";
+        final String errorMessage = "Invalid query: ";
+
+        final Throwable thrown = catchThrowable(
+                () -> LktJenaReporter.runReport(this.testRdfFile.getAbsolutePath(), invalidQuery, outFile, ""));
+        assertThat(this.outStream.toString()).contains(errorMessage);
+        assertThat(thrown).isInstanceOf(QueryParseException.class);
+    }
+
+}
