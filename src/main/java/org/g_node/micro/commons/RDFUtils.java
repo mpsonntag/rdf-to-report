28261a7be7e316ee72ce03eeced0402478bcb3e2
M. Sonntag <michael.p.sonntag@gmail.com>
2016-03-16 17:28:01 +0100

    RDFUtils: Remove RDF namespace constants

--- src/main/java/org/g_node/micro/commons/RDFUtils.java
+++ src/main/java/org/g_node/micro/commons/RDFUtils.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2016, German Neuroinformatics Node (G-Node)
+ * Copyright (c) 2015, German Neuroinformatics Node (G-Node)
  *
  * All rights reserved.
  *
@@ -12,10 +12,6 @@ package org.g_node.micro.commons;
 
 import com.hp.hpl.jena.rdf.model.Property;
 import com.hp.hpl.jena.rdf.model.Resource;
-import com.hp.hpl.jena.vocabulary.DCTerms;
-import com.hp.hpl.jena.vocabulary.RDF;
-import com.hp.hpl.jena.vocabulary.RDFS;
-import com.hp.hpl.jena.vocabulary.XSD;
 
 /**
  * Class contains small helper functions when dealing with Jena RDF models.
@@ -23,54 +19,6 @@ import com.hp.hpl.jena.vocabulary.XSD;
  * @author Michael Sonntag (sonntag@bio.lmu.de)
  */
 public final class RDFUtils {
-    /**
-     * Namespace used to identify RDF resources.
-     */
-    public static final String RDF_NS_RDF = RDF.getURI();
-    /**
-     * RDF namespace prefix.
-     */
-    public static final String RDF_NS_RDF_ABR = "rdf";
-    /**
-     * Namespace used to identify RDFS resources.
-     */
-    public static final String RDF_NS_RDFS = RDFS.getURI();
-    /**
-     * RDFS namespace prefix.
-     */
-    public static final String RDF_NS_RDFS_ABR = "rdfs";
-    /**
-     * Namespace used to identify XSD resources.
-     */
-    public static final String RDF_NS_XSD = XSD.getURI();
-    /**
-     * XSD namespace prefix.
-     */
-    public static final String RDF_NS_XSD_ABR = "xs";
-    /**
-     * Namespace used to identify FOAF RDF resources.
-     */
-    public static final String RDF_NS_FOAF = "http://xmlns.com/foaf/0.1/";
-    /**
-     * FOAF Namespace prefix.
-     */
-    public static final String RDF_NS_FOAF_ABR = "foaf";
-    /**
-     * Namespace used to identify Dublin core resources.
-     */
-    public static final String RDF_NS_DC = DCTerms.NS;
-    /**
-     * Dublin core Namespace prefix.
-     */
-    public static final String RDF_NS_DC_ABR = "dc";
-    /**
-     * Core Ontology for neuroscientific metadata defined by the G-Node.
-      */
-    public static final String RDF_NS_GN_ONT = "https://github.com/G-Node/neuro-ontology/";
-    /**
-     * G-Node ontology namespace prefix.
-     */
-    public static final String RDF_NS_GN_ONT_ABR = "gn";
     /**
      * Method adds an RDF Literal only to a Jena Resource, if the Literal String
      * actually contains a value.
@@ -83,4 +31,5 @@ public final class RDFUtils {
             res.addLiteral(p, litStr);
         }
     }
+
 }
